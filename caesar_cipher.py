6586a06ddd997f5462739dbf21f8314f9435e19e
Галина Лотонина <CheckB0x@gmail.com>
2020-10-24 14:49:21 +0300

    Added decoding function

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,7 +1,8 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+shift = 13
 
-def caesar_cipher_encryption(encryption_text):
+def caesar_cipher_encryption(encryption_text, shift):
     caesar_cipher_list = []
     for i in encryption_text:
         if i in alphabet:
@@ -20,9 +21,37 @@ def caesar_cipher_encryption(encryption_text):
     caesar_cipher_text = "".join(caesar_cipher_list)
     return caesar_cipher_text
 
+def caesar_cipher_decryption(decryption_text, shift):
+    caesar_cipher_list = []
+    for i in decryption_text:
+        if i in alphabet:
+            index = alphabet.index(i)
+            index = int((index - shift) % 26)
+            i = alphabet[index]
+            caesar_cipher_list += i
+        elif i in ALPHABET:
+            index = ALPHABET.index(i)
+            index = int((index - shift) % 25)
+            i = ALPHABET[index]
+            caesar_cipher_list += i
+        else:
+            i = i
+            caesar_cipher_list += i
+    caesar_cipher_text = "".join(caesar_cipher_list)
+    return caesar_cipher_text
 
-encryption_text = input("Enter text for encryption: ")
-shift = int(input("Enter shift(0-25): "))
 
-encrypter = caesar_cipher_encryption(encryption_text)
-print(encrypter)
+def all_shift(cipher):
+    if cipher == "d":
+        text = input("Enter text for decryption: ")
+        function = caesar_cipher_decryption
+        print("I decoded your text")
+    elif cipher == "e":
+        text = input("Enter text for encryption: ")
+        function = caesar_cipher_encryption
+        print("I encoded your text")
+    cipher = function(text, shift)
+    print(f"In Rot13: {cipher}")
+
+if __name__ == "__main__":
+    all_shift(cipher = input("Encrypted(e) or Decrypted(d)?\n"))
