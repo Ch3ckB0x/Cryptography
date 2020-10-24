4520a81ad17227baf0431798412f4495a46d7a5f
Галина Лотонина <CheckB0x@gmail.com>
2020-10-24 13:17:35 +0300

    test

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,7 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-# caesar_cipher_encryption
 def caesar_cipher_encryption(encryption_text):
     caesar_cipher_list = []
     for i in encryption_text:
